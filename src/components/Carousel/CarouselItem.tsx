c8b069e77aa04bc55121f85a948b5f4646f57cde
runefaeroe <rune.faro@bkk.no>
2024-01-21 14:57:59 +0100

    feat: create carousel item component with hoverstyle

---
+++ src/components/Carousel/CarouselItem.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export const CarouselItem = ({
+  title,
+  poster,
+}: {
+  title: string;
+  poster: string;
+}) => {
+  return (
+    <div className="relative basis-full flex-grow md:basis-1/2 lg:basis-1/3 xl:basis-1/4 flex-shrink-0 flex justify-center">
+      <div className="relative group cursor-pointer w-full h-full object-cover">
+        <img
+          src={poster}
+          alt={title}
+          className="w-full h-full object-cover transition-transform transform group-hover:scale-105"
+        />
+
+        <div className="p-4 w-full h-full absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-hover:scale-105 bg-black bg-opacity-50 ">
+          <p className="text-white text-lg font-bold">{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
