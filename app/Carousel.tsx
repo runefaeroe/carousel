3aea69cc7a870407c02c00cd469c3f32722d4860
runefaeroe <rune.faro@bkk.no>
2024-01-21 11:00:38 +0100

    feat: pass movie data to carousel client component

---
+++ app/Carousel.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Movies } from "./src/utils/getRandomMovies";
+
+const Carousel = ({ movies }: { movies: Movies[] }) => {
+  return (
+    <div>
+      <h1>Carousel</h1>
+    </div>
+  );
+};
+
+export default Carousel;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import Carousel from "./Carousel";
+import { fetchRandomMovies } from "./src/utils/getRandomMovies";
+
+export default async function Home() {
+  const movies = await fetchRandomMovies();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Movie carousel</h1>
+      <Carousel movies={movies} />
     </main>
   );
 }
